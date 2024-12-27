207c5cb7f5db70e1a0d4a5a0b5d846142ffedc44
Gulmohar <92192914+vvihar@users.noreply.github.com>
2024-12-27 15:05:02 +0900

    fix code

--- MDCVRP/evaluation.py
+++ MDCVRP/evaluation.py
@@ -1,49 +1,42 @@
-import torch
 import os
-import torch.nn.functional as F
-from torch.utils.data import Dataset
-from scipy.stats import ttest_rel
-import copy
-import  tqdm
-import numpy as np
-#from TSPlIbInstance import creat_data,reward
-from collections import OrderedDict
-from collections import namedtuple
-from itertools import product
-from creat_vrp import reward1,creat_data,reward,creat_instance
 import time
+
+# from TSPlIbInstance import create_data,reward
 from collections import OrderedDict
+
+import torch
+from create_vrp import create_data, reward1
 from VRP_Actor import Model
 
-from torch.nn import DataParallel
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-from torch_geometric.data import Data,DataLoader
-def rollout(model, dataset,T=1,Sample=False):
+
+def rollout(model, dataset, T=1, Sample=False):
     # Put in greedy evaluation mode!
     model.eval()
+
     def eval_model_bat(bat):
         with torch.no_grad():
+            cost, _ = model(bat, n_nodes * 2, Sample, T)
 
-            cost, _ = model(bat, n_nodes * 2,Sample,T)
-
-            cost1 = reward1(bat.x,cost.detach(), n_nodes)
+            cost1 = reward1(bat.x, cost.detach(), n_nodes)
         return cost1.cpu()
-    totall_cost = torch.cat([eval_model_bat(bat.to(device))for bat in dataset], 0)
+
+    totall_cost = torch.cat([eval_model_bat(bat.to(device)) for bat in dataset], 0)
     return totall_cost
 
-def evaliuate(valid_loder,T=1,Sample=False):
 
-    agent = Model(3, 128, 1, 16, conv_laysers=4).to(device)
+def evaliuate(valid_loder, T=1, Sample=False):
+    agent = Model(3, 128, 1, 16, conv_layers=4).to(device)
     agent.to(device)
 
-    folder = 'Vrp-22-GAT'
-    filename = 'rollout'
+    folder = "Vrp-22-GAT"
+    filename = "rollout"
     filepath = os.path.join(folder, filename)
-    path = os.path.join(filepath, '%s' % 22)
+    path = os.path.join(filepath, "%s" % 22)
     if os.path.exists(path):
-        path1 = os.path.join(path, 'actor.pt')
-        state_dict = torch.load(path1,map_location='cuda:0')
+        path1 = os.path.join(path, "actor.pt")
+        state_dict = torch.load(path1, map_location="cuda:0")
         new_state_dict = OrderedDict()
         print(1)
         for k, v in state_dict.items():
@@ -52,24 +45,25 @@ def evaliuate(valid_loder,T=1,Sample=False):
 
         agent.load_state_dict(new_state_dict)
 
-    start=time.time()
-    cost = rollout(agent, valid_loder, T,Sample)
-    end=time.time()-start
+    start = time.time()
+    cost = rollout(agent, valid_loder, T, Sample)
+    end = time.time() - start
 
     cost1 = cost.mean()
-    cost2=cost.min()
+    cost2 = cost.min()
     if Sample:
-        print(cost1, 'greedy----Gap', 100 * (cost1 - 6.1) / 6.1)
+        print(cost1, "greedy----Gap", 100 * (cost1 - 6.1) / 6.1)
     else:
-        print(cost2, 'Sampleing----Gap','---------',T, 100 * (cost2 - 6.1) / 6.1)
+        print(cost2, "Sampleing----Gap", "---------", T, 100 * (cost2 - 6.1) / 6.1)
 
-    return cost1,cost2
-greedy=True
+    return cost1, cost2
+
+
+greedy = True
 n_nodes = 22
-batch_size=256
+batch_size = 256
 n = []
-tours=[]
-valuate_size=10
-valid_loder = creat_data(n_nodes,valuate_size , batch_size=batch_size)
-_, sample_cost = evaliuate(valid_loder, 1,greedy)
-
+tours = []
+valuate_size = 10
+valid_loder = create_data(n_nodes, valuate_size, batch_size=batch_size)
+_, sample_cost = evaliuate(valid_loder, 1, greedy)
--- MDCVRP/vrpUpdate.py
+++ MDCVRP/vrpUpdate.py
@@ -1,32 +1,38 @@
 import torch
-import time
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def update_state(demand,dynamic_capcity,selected,num_depot,c=20):#dynamic_capcity(num,1)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    #Is there a group to access the depot
 
-    current_demand = torch.gather(demand,1,selected)
+def update_state(
+    demand, dynamic_capacity, selected, num_depot, c=20
+):  # dynamic_capacity(num,1)
+    # Is there a group to access the depot
 
-    dynamic_capcity = dynamic_capcity-current_demand
+    current_demand = torch.gather(demand, 1, selected)
+
+    dynamic_capacity = dynamic_capacity - current_demand
 
     for j in range(num_depot):
         depot = selected.squeeze(-1).eq(j)
 
         if depot.any():
-            dynamic_capcity[depot.nonzero().squeeze()] = c
+            dynamic_capacity[depot.nonzero().squeeze()] = c
 
-    return dynamic_capcity.detach()#(bach_size,1)
+    return dynamic_capacity.detach()  # (bach_size,1)
 
 
-def update_mask(demand,capcity,selected,mask,i,num_depot):
-    go_depot = torch.zeros((selected.size(0)),dtype=torch.bool).to(device)
+def update_mask(demand, capacity, selected, mask, i, num_depot):
+    go_depot = torch.zeros((selected.size(0)), dtype=torch.bool).to(device)
     for j in range(num_depot):
-        go_depo = selected.squeeze(-1).eq(j)#If there is a route to select a depot, mask the depot, otherwise it will not mask the depot
+        go_depo = selected.squeeze(
+            -1
+        ).eq(
+            j
+        )  # If there is a route to select a depot, mask the depot, otherwise it will not mask the depot
         go_depot += go_depo
 
-    #print(go_depot.nonzero().squeeze())
-    #visit = selected.ne(0)
+    # print(go_depot.nonzero().squeeze())
+    # visit = selected.ne(0)
 
     mask1 = mask.scatter(1, selected.expand(mask.size(0), -1), 1)
 
@@ -38,13 +44,13 @@ def update_mask(demand,capcity,selected,mask,i,num_depot):
         for k in range(num_depot):
             mask1[(go_depot).nonzero(), k] = 1
 
-    if i+num_depot>demand.size(1):
+    if i + num_depot > demand.size(1):
         is_done = (mask1[:, num_depot:].sum(1) >= (demand.size(1) - num_depot)).float()
         combined = is_done.gt(0)
         mask1[combined.nonzero(), 0] = 0
 
-    a = demand>capcity
+    a = demand > capacity
 
     mask = a + mask1
 
-    return mask.detach(),mask1.detach()
+    return mask.detach(), mask1.detach()
